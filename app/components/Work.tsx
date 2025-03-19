7c36ccbe47bf5d7723ef875ff6e9703409633bc8
Gurkirt Heerey <gheerey12@gmail.com>
2025-03-19 13:33:36 -0700

    Add Work component and portfolio images

---
+++ app/components/Work.tsx
@@ -0,0 +1,48 @@
+import { assets, workData } from "@/assets/assets";
+import Image from "next/image";
+
+const Work = () => {
+  return (
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
+      <h2 className="text-center text-5xl font-Ovo">My latest work</h2>
+      <p className="text-center mx-auto max-w-2xl mt-5 mb-12 font-Ovo">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      </p>
+
+      <div className="grid grid-cols-auto gap-5 my-10">
+        {workData.map(({ bgImage, title, description, link }, index) => (
+          <div
+            key={index}
+            style={{ backgroundImage: `url(${bgImage})` }}
+            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
+          >
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-300 group-hover:bottom-7">
+              <div>
+                <h2 className="font-semibold">{title}</h2>
+                <p className="text-sm text-gray-700">{description}</p>
+              </div>
+              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_0_#000] duration-300 group-hover:bg-lime-300 transition">
+                <Image src={assets.send_icon} alt="send" className="w-5" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <a
+        href=""
+        className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full y-3 px-10 mx-auto my-20 hover:bg-lightHover duration-300"
+      >
+        Show more
+        <Image
+          src={assets.right_arrow_bold}
+          alt="right-arrow-bold"
+          className="w-4"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default Work;
